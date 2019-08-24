37db3296959db90e16203e4e3792ea07d914b2aa
KornelKopanski <kkornel1771@gmail.com>
2019-08-24 19:12:01 +0200

    Utworzono funkcję dodającą użytkownika oraz hasła.

--- exercies/password_manager.py
+++ exercies/password_manager.py
@@ -1,2 +1,17 @@
 
 PASSWORD = {"user":None,"password":None,"passwords":{}}
+
+def register():
+    user = input("Podaj nazwę użtkownika:")
+    password = input("Podaj hasło:")
+    PASSWORD["user"] = user
+    PASSWORD["password"] = password
+
+register()
+
+def add_password(item,password):
+
+    PASSWORD["passwords"][item] = password
+
+
+
