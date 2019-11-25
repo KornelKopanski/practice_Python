0296629559b30179b7d8cca1a7a4b4994bc6ee15
KornelKopanski <kkornel1771@gmail.com>
2019-11-25 17:12:49 +0100

    Prosty programik do zapisu informacji o firmach.

--- exer/com_it.py
+++ exer/com_it.py
@@ -3,7 +3,7 @@ from exer.frame_com_it import *
 
 
 root = Tk()
-root.geometry("300x200")
+root.geometry("300x250")
 
 fr = Wi(root)
 
--- exer/frame_com_it.py
+++ exer/frame_com_it.py
@@ -11,41 +11,33 @@ class Wi(Frame):
         self.grid()
         self.create_label()
         self.create_entry()
-        self.create_combobox()
+        self.create_button()
 
     def create_label(self):
 
-        self.city = Label(self,text="Miasto")
-        self.city.grid(row=0,column=0)
-        self.name_company = Label(self,text="Nazwa firmy")
-        self.name_company.grid(row=1,column=0)
-        self.site_company = Label(self,text="Strona WWW")
-        self.site_company.grid(row=2,column=0)
-        self.status = Label(self,text = "Status")
-        self.status.grid(row=3,column=0)
-        self.post_date = Label(self,text="Data aplikacji")
-        self.post_date.grid(row=4,column=0)
-        self.post_date_info = Label(self, text=str(date.today()), bg="white")
-        self.post_date_info.grid(row=4, column=1)
-        self.description = Label(self,text="Uwagi")
-        self.description.grid(row=5,column=0)
 
-    def create_combobox(self):
-        self.category_combobox = ttk.Combobox(self, values=["Lublin", "Warszawa"])
-        self.category_combobox.current(0)
-        self.category_combobox.grid(row=0, column=1)
-        self.status_combobox = ttk.Combobox(self,values=["Do wysłania","Wysłane","Feedback Positive","Feedback Negative"])
-        self.status_combobox.current(0)
-        self.status_combobox.grid(row=3,column=1)
+        self.site_company = Label(self,text="Strona WWW")
+        self.site_company.grid(row=0,column=0)
+
+        self.description = Label(self,text="Uwagi")
+        self.description.grid(row=1,column=0)
 
     def create_entry(self):
 
-        self.name_company_entry = Entry(self)
-        self.name_company_entry.grid(row=1,column=1)
-        self.site_company_entry = Entry(self)
-        self.site_company_entry.grid(row=2,column=1)
-        self.description_entry = Entry(self)
-        self.description_entry.grid(row=5,column=1)
+
+        self.site_company_entry = Entry(self,width = 33)
+        self.site_company_entry.grid(row=0,column=1)
+
+        self.description_entry = Text(self,width = 25, height = 10)
+        self.description_entry.grid(row=1,column=1)
+
+    def create_button(self):
+
+        self.save = Button(self,text="Zapisz dane", width = 25)
+        self.save.grid(row=2,column=1)
+
+        self.save = Button(self, text="Pokaż dane", width = 25)
+        self.save.grid(row=3, column=1)
 
 
 
