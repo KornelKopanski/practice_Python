42367a3388335e31ab2bdfa41d6902e04eb98be2
KornelKopanski <kkornel1771@gmail.com>
2019-11-25 17:47:24 +0100

    Prosty programik do zapisu informacji o firmach.

--- exer/com_it.py
+++ exer/com_it.py
@@ -3,7 +3,7 @@ from exer.frame_com_it import *
 
 
 root = Tk()
-root.geometry("300x250")
+root.geometry("300x300")
 
 fr = Wi(root)
 
--- exer/frame_com_it.py
+++ exer/frame_com_it.py
@@ -1,5 +1,6 @@
 
 from tkinter import *
+from tkinter import ttk
 
 class Wi(Frame):
 
@@ -10,32 +11,48 @@ class Wi(Frame):
         self.create_label()
         self.create_entry()
         self.create_button()
+        self.create_combobox()
 
     def create_label(self):
 
 
         self.site_company = Label(self,text="Strona WWW")
-        self.site_company.grid(row=0,column=0)
+        self.site_company.grid(row=1,column=0)
 
         self.description = Label(self,text="Uwagi")
-        self.description.grid(row=1,column=0)
+        self.description.grid(row=2,column=0)
+
+        self.city = Label(self,text="Miasto")
+        self.city.grid(row=0,column=0)
+
+        self.status = Label(self,text="Status")
+        self.status.grid(row=3,column=0)
 
     def create_entry(self):
 
 
         self.site_company_entry = Entry(self,width = 33)
-        self.site_company_entry.grid(row=0,column=1)
+        self.site_company_entry.grid(row=1,column=1)
 
         self.description_entry = Text(self,width = 25, height = 10)
-        self.description_entry.grid(row=1,column=1)
+        self.description_entry.grid(row=2,column=1)
+
+    def create_combobox(self):
+
+        self.category_combobox = ttk.Combobox(self, values=["Lublin", "Warszawa"],width = 30)
+        self.category_combobox.current(0)
+        self.category_combobox.grid(row=0, column=1)
+        self.status_combobox = ttk.Combobox(self,values=["Do wysłania","Wysłane","Feedback Positive","Feedback Negative"],width = 30)
+        self.status_combobox.current(0)
+        self.status_combobox.grid(row=3,column=1)
 
     def create_button(self):
 
         self.save = Button(self,text="Zapisz dane", width = 25)
-        self.save.grid(row=2,column=1)
+        self.save.grid(row=4,column=1)
 
         self.save = Button(self, text="Pokaż dane", width = 25)
-        self.save.grid(row=3, column=1)
+        self.save.grid(row=5, column=1)
 
 
 
